7ec1930de12aaae83e1c1ae17d24623197e7610f
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-12-12 03:19:13 +0600

    added

--- src/pages/Projects/Mern/Mobile.js
+++ src/pages/Projects/Mern/Mobile.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { ExternalLink } from 'react-external-link';
 import mobile from '../../../components/projectsImage/mobile.jpg';
-
+import img1 from '../../../components/mern/mobile/img-1.png';
+import img2 from '../../../components/mern/mobile/img-2.png';
+import img3 from '../../../components/mern/mobile/img-3.png';
+import img4 from '../../../components/mern/mobile/img-4.png';
+import img5 from '../../../components/mern/mobile/img-5.png';
+import img6 from '../../../components/mern/mobile/img-6.png';
+import img7 from '../../../components/mern/mobile/img-7.png';
+import img8 from '../../../components/mern/mobile/img-8.png';
+import img9 from '../../../components/mern/mobile/img-9.png';
+import img10 from '../../../components/mern/mobile/img-10.png';
+import img11 from '../../../components/mern/mobile/img-11.png';
+import img12 from '../../../components/mern/mobile/img-12.png';
+import img13 from '../../../components/mern/mobile/img-13.png';
+import img14 from '../../../components/mern/mobile/img-14.png';
+import img15 from '../../../components/mern/mobile/img-15.png';
+import img16 from '../../../components/mern/mobile/img-16.png';
+import img17 from '../../../components/mern/mobile/img-17.png';
+import img18 from '../../../components/mern/mobile/img-18.png';
+import img19 from '../../../components/mern/mobile/img-19.png';
+import img20 from '../../../components/mern/mobile/img-20.png';
+import img21 from '../../../components/mern/mobile/img-21.png';
+import img22 from '../../../components/mern/mobile/img-22.png';
+import img23 from '../../../components/mern/mobile/img-23.png';
+import img24 from '../../../components/mern/mobile/img-24.png';
+import img25 from '../../../components/mern/mobile/img-25.png';
 const Mobile = () => {
     return (
         <div className='w-full'>
@@ -9,11 +33,11 @@ const Mobile = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content grid grid-cols-1 lg:grid-cols-2">
                     <div className='w-full'>
-                        <img src={mobile} className="rounded-lg shadow-2xl w-full" alt=''
+                        <img src={img1} className="rounded-lg shadow-2xl w-full" alt=''
                         />
                     </div>
                     <div className='w-full'>
-                        <h1 className="text-5xl font-bold">MOBILE RESELL</h1>
+                        <h1 className="text-2xl lg:text-5xl font-bold">MOBILE RESELL</h1>
                         <div className="py-6">
                             <div>
                                 <h2 className='text-primary text-justify'>FEATURES</h2>
@@ -55,7 +79,87 @@ const Mobile = () => {
                 </div>
             </div>
             {/* .....hotel..... */}
-
+            <div className='text-2xl lg:text-3xl font-bold text-black bg-warning w-full h-20 flex flex-col place-items-center justify-center items-center'>
+                <h2>Home Page Description</h2>
+            </div>
+            <div>
+                <img src={img2} alt="" />
+            </div>
+            <div>
+                <img src={img3} alt="" />
+            </div>
+            <div>
+                <img src={img4} alt="" />
+            </div>
+            <div>
+                <img src={img5} alt="" />
+            </div>
+            <div>
+                <img src={img6} alt="" />
+            </div>
+            <div className='w-full flex flex-col justify-center items-center place-items-center w-full text-2xl lg:text-3xl text-white bg-black font-bold text-start h-16 lg:h-24'>
+                <h2 className='w-full'>Admin Dashboard</h2>
+            </div>
+            <div>
+                <img src={img7} alt="" />
+            </div>
+            <div>
+                <img src={img8} alt="" />
+            </div>
+            <div>
+                <img src={img9} alt="" />
+            </div>
+            <div>
+                <img src={img10} alt="" />
+            </div>
+            <div>
+                <img src={img11} alt="" />
+            </div>
+            <div>
+                <img src={img12} alt="" />
+            </div>
+            <div>
+                <img src={img13} alt="" />
+            </div>
+            <div>
+                <img src={img14} alt="" />
+            </div>
+            <div>
+                <h2 className='flex flex-col place-items-center justify-center items-center text-xl lg:text-3xl font-bold text-warning bg-black w-full h-12 lg:h-24'>User Dashboard</h2>
+            </div>
+            <div className='flex w-full'>
+                <img src={img24} alt="" className='w-1/2' />
+                <img src={img25} alt="" className='w-1/2' />
+            </div>
+            <div>
+                <img src={img15} alt="" />
+            </div>
+            <div className='w-full'>
+                <h2 className='flex flex-col place-items-center justify-center items-center text-xl lg:text-3xl font-bold text-warning bg-black w-full h-12 lg:h-24'>User Payment Process</h2>
+                <img src={img16} alt="" />
+            </div>
+            <div>
+                <img src={img17} alt="" />
+            </div>
+            <div>
+                <img src={img18} alt="" />
+            </div>
+            <div>
+                <h2 className='flex flex-col place-items-center justify-center items-center text-xl lg:text-3xl font-bold text-warning bg-black w-full h-12 lg:h-24'>Seller Dashboard</h2>
+            </div>
+            <div className='flex w-full'>
+                <img src={img22} alt="" className='w-1/2' />
+                <img src={img23} alt="" className='w-1/2' />
+            </div>
+            <div>
+                <img src={img19} alt="" />
+            </div>
+            <div>
+                <img src={img20} alt="" />
+            </div>
+            <div>
+                <img src={img21} alt="" />
+            </div>
         </div>
     );
 };
